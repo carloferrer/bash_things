69be391cfa249a5206e81e4eb3fc082afcc6bdb5
carloferrer <thecarloferrer@gmail.com>
2019-02-26 09:30:04 -0500

    Add 'mcd' script

--- bash_scripts.sh
+++ bash_scripts.sh
@@ -1,6 +1,7 @@
 #################
 # HANDY SCRIPTS #
 #################
+mcd() { mkdir -pv "$@" ; cd "$@" ; }
 jira() { /usr/bin/open -a "/Applications/Google Chrome.app" "https://groupby.atlassian.net/browse/$1" ; }
 sfpr() { /usr/bin/open -a "/Applications/Google Chrome.app" "https://github.com/groupby/storefront/pull/$1" ; }
 sfbr() { /usr/bin/open -a "/Applications/Google Chrome.app" "https://github.com/groupby/storefront/tree/$1" ; }
