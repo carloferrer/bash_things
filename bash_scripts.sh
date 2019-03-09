796a696c76a7da6c37229cbbfc3a86e4c5b2ff09
Carlo <thecarloferrer@gmail.com>
2019-03-09 12:13:42 -0500

    Correct scripts for EoF errors

--- bash_scripts.sh
+++ bash_scripts.sh
@@ -4,9 +4,9 @@
 mcd() { mkdir -pv "$@" ; cd "$@" ; }
 
 # Git branch stuff.
-parse_git_branch() { git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1/' }
+parse_git_branch() { git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1/' ; }
 
-gpu() { git push -u origin $(parse_git_branch) }
+gpu() { git push -u origin $(parse_git_branch) ; }
 
 jira() { /usr/bin/open -a "/Applications/Google Chrome.app" "https://groupby.atlassian.net/browse/$1" ; }
 sfpr() { /usr/bin/open -a "/Applications/Google Chrome.app" "https://github.com/groupby/storefront/pull/$1" ; }
@@ -15,5 +15,5 @@ thsrs() { /usr/bin/open -a "/Applications/Google Chrome.app" "https://www.thesau
 
 ylsf() { yarn link @storefront/$1 ; }
 yusf() { yarn unlink @storefront/$1 ; }
-yd() { (cd ~/Workspace/storefront/packages/@storefront/$1 && yarn dev ;) }
-yt() { (cd ~/Workspace/storefront/packages/@storefront/$1 && yarn tdd ;) }
+yd() { (cd ~/Workspace/storefront/packages/@storefront/$1 && yarn dev ;) ; }
+yt() { (cd ~/Workspace/storefront/packages/@storefront/$1 && yarn tdd ;) ; }
