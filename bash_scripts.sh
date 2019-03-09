1ffab7c06ae9649ade43c8cbd75d2837d40c0755
Carlo <thecarloferrer@gmail.com>
2019-03-09 11:47:30 -0500

    Add 'gpu' script

--- bash_scripts.sh
+++ bash_scripts.sh
@@ -4,9 +4,9 @@
 mcd() { mkdir -pv "$@" ; cd "$@" ; }
 
 # Git branch stuff.
-parse_git_branch() {
-  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1/'
-}
+parse_git_branch() { git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1/' }
+
+gpu() { git push -u origin $(parse_git_branch) }
 
 jira() { /usr/bin/open -a "/Applications/Google Chrome.app" "https://groupby.atlassian.net/browse/$1" ; }
 sfpr() { /usr/bin/open -a "/Applications/Google Chrome.app" "https://github.com/groupby/storefront/pull/$1" ; }
