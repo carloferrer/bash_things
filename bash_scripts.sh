76f939532d13e03b30fe960899d4d2ddf467e7d4
Carlo <thecarloferrer@gmail.com>
2019-04-06 16:35:10 -0400

    Add legacy repo cloning script

--- bash_scripts.sh
+++ bash_scripts.sh
@@ -8,6 +8,17 @@ parse_git_branch() { git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/
 
 gpu() { git push -u origin $(parse_git_branch) ; }
 
+# Easier cloning of legacy repos.
+nomod_clone() {
+  git clone "git@github.com:carloferrer/$1"
+  if [ -d "node_modules" ]; then
+    printf "\nDirectory 'node_modules' found ... removing 'node_modules'.\n\n"
+    rm -rf "node_modules"
+  else
+    printf "\nNo directory 'node_modules' found.\n\n"
+  fi
+}
+
 # jira() { /usr/bin/open -a "/Applications/Google Chrome.app" "https://groupby.atlassian.net/browse/$1" ; }
 # sfpr() { /usr/bin/open -a "/Applications/Google Chrome.app" "https://github.com/groupby/storefront/pull/$1" ; }
 # sfbr() { /usr/bin/open -a "/Applications/Google Chrome.app" "https://github.com/groupby/storefront/tree/$1" ; }
